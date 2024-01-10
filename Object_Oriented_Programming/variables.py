cf48658caa31e6f29b6ff7bf1ea03d7a4b0a6420
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-01-10 13:56:38 +0530

    feat: Types of Variables in OOPS

---
+++ Object_Oriented_Programming/variables.py
@@ -0,0 +1,17 @@
+# Types of Variables in OOPS
+class Vehicles:
+    wheel = 4          # Class Variables
+    def __init__(self,):      
+        self.mil = 10         #Instance Variables
+        self.name = "BMW"
+    
+    
+
+
+v1 = Vehicles()
+v2 = Vehicles()    
+Vehicles.wheel = 6 
+v1.mil = 8            #Instance Variables
+v1.name ="Audi"
+print(v1.mil,v1.name,Vehicles.wheel)
+print(v2.mil,v2.name,Vehicles.wheel)
