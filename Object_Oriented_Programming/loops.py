3f96a564549ca996de6585834266c4d6e0cb1c80
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-01-11 22:59:41 +0530

    feat:loops

---
+++ Object_Oriented_Programming/loops.py
@@ -0,0 +1,16 @@
+
+#3. Break Statement Candy machine problem
+x = int(input("Enter number of candies:"))
+av = 10
+c=1
+while c<=x:
+    if c > av:
+        break
+    print("candies")
+    c+=1
+
+#4. Continue Statement print number of odd number upto 10
+for i in range(1,11):
+    if i%2==0:
+        continue
+    print(i)
