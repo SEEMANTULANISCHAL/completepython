5c040dc82d438196477f080a442336daf54b0d84
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-01-12 12:36:38 +0530

    feat:patterns in for

--- Object_Oriented_Programming/patterns.py
+++ Object_Oriented_Programming/patterns.py
@@ -1,4 +1,4 @@
-#1. Print Pattern 4X4 #
+#1. Print Pattern 4X4 # using while loop
 i=1
 while i<=4:
     print("# ", end="")
@@ -7,4 +7,10 @@ while i<=4:
         print("# ", end="")
         j+=1
     i+=1
+    print()
+
+#2.  Print Pattern 4X4 # using for loop
+for j in range(4):
+    for i in range(4):
+        print("# ", end="")
     print()
