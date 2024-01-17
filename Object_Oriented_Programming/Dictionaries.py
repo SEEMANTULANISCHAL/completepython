12768b62595c3b6d264c0b08839e40f915bcab06
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-01-17 12:59:18 +0530

    feat:Dictionaries

---
+++ Object_Oriented_Programming/Dictionaries.py
@@ -0,0 +1,24 @@
+#Intializing Students in classroom
+
+classroom ={
+    "Nischal": 90,
+    "Swarna" : 95,
+    "Namratha" : 93
+}
+
+#Append more students in Classroom
+
+classroom["Julie"] = 92
+classroom["Kate"] = 91
+print(classroom)
+
+#Iterate Students in classroom
+
+for names,marks in classroom.items():
+    print("The student is %s and grade is %d" % (names,marks))
+
+#Delete Kate and Instead add "Jack" with 92 marks
+
+del classroom["Kate"]
+classroom["Jack"] = 92
+print(classroom)
