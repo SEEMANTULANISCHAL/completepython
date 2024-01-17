9c80e9326a8ecc66d43dc51a7f00791325ee068d
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-01-17 13:17:01 +0530

    feat:Problem7

---
+++ Hacker_Rank/Problem7.py
@@ -0,0 +1,17 @@
+#Nested List
+records=[]
+list_name=[]
+list_grade=[]
+for i in range(int(input())):
+    name = input()
+    grade = float(input())
+    records.append([name,grade])
+    list_grade.append(grade)
+set_grade=list(set(list_grade))
+set_grade.sort()
+second_least = set_grade[1]
+#LIST COMPREHENSION
+ouput=[i[0] for i in records if i[1] == second_least]
+ouput.sort()
+for x in ouput:
+    print(x)
