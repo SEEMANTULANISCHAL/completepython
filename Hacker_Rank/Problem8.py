b7842718cb66a68cb205ab5571e164ced7872cf0
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-01-19 13:40:06 +0530

    feat: problem8

---
+++ Hacker_Rank/Problem8.py
@@ -0,0 +1,10 @@
+n=int(input())
+student_mark ={}
+for i in range(n):
+    name, *line = input().split()
+    mark = list(map(float,line))
+    student_mark[name] = mark
+selected_name = input()
+l1 = list(student_mark[selected_name])
+avg = sum(l1)/3
+print("%0.2f" %(avg))
