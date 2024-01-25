475e354449d8386f2a42cf1732e6122e313f9844
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-01-25 11:05:50 +0530

    update: Iterator Changes

--- Object_Oriented_Programming/Iterator.py
+++ Object_Oriented_Programming/Iterator.py
@@ -1,6 +1,23 @@
-#ITERATOR
-list = [3,4,5,6,7]
-
+#ITERATOR INBUILT
+list =[2,3,4,5]
 it = iter(list)
 print(it.__next__())
-print(it.__next__())
+print(next(it))
+# Create own Iterator
+#Print top 10 values
+class Values:
+    def __init__(self):
+        self.value = 1 
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.value <= 10:
+            num = self.value
+            self.value += 1 
+            return num  
+        else:
+            raise StopIteration
+v1 = Values()
+print(next(v1))
+for i in v1:
+    print(i)
