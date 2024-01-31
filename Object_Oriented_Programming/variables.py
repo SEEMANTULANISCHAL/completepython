458f88d9a7fd21a48aff8b794cec83deedfccb2a
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-01-31 23:55:52 +0530

    update:variables

--- Object_Oriented_Programming/variables.py
+++ Object_Oriented_Programming/variables.py
@@ -9,9 +9,11 @@ class Vehicles:
 
 
 v1 = Vehicles()
-v2 = Vehicles()    
+v2 = Vehicles()   
+v3 = Vehicles() 
 Vehicles.wheel = 6 
 v1.mil = 8            #Instance Variables
 v1.name ="Audi"
 print(v1.mil,v1.name,Vehicles.wheel)
 print(v2.mil,v2.name,Vehicles.wheel)
+
