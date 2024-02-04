0e8b0a9d86a4bc6553bda4247474e65f9f3e8e8d
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-02-04 23:26:21 +0530

    update:function

--- Object_Oriented_Programming/Function.py
+++ Object_Oriented_Programming/Function.py
@@ -5,11 +5,13 @@ function()
 
 #With Aarguments
 def function_arg(Name,age):
-    print("My name is %s and age is %d"%(Name,age))
-function_arg("Nischal",21)
+    return "My name is %s and age is %d"%(Name,age)
+x = function_arg("Nischal",21)
+print(x)
 
 #Sum 
 def sum(a,b):
     return a+b
 x=sum(3,4)
 print(x)
+
