478f877221e5141f4ed9706a4c3677f7e3fcfc69
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-02-06 19:13:14 +0530

    update:guess_num_user

---
+++ Project.py/guess_number_user.py
@@ -0,0 +1,18 @@
+import random 
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low,high) 
+        else:
+            guess = high
+        feedback = input(f"Is {guess} is too high (H) or guess is too low (L) or guess is correct (C)?").lower()
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print("the computer guesses right")
+computer_guess(10)            
