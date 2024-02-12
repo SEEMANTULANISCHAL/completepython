a57405dee7012dac0df1c58b87a75f2f3084ea80
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-02-12 22:24:49 +0530

    update:nested

---
+++ Object_Oriented_Programming/nested_for.py
@@ -0,0 +1,15 @@
+Students = []
+num = int(input())
+for i in range(num):
+    name = str(input())
+    grade = float(input())
+    list = [name, grade]
+    Students.append(list)
+print(Students)
+
+scores = []
+names = []
+
+for student in Students:
+    
+    print(name)
