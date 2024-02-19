99129d9dc5ddc17a6f15ce3ad45cbcdd571efd85
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-02-19 21:55:00 +0530

    update:suii

--- Project.py/madlibs.py
+++ Project.py/madlibs.py
@@ -3,5 +3,5 @@ noun = input("nonun: ")
 symbol = input("symbol: ")
 sport = input("sport: ")
 adjective = input("adjective: ")
-madlibs = f" I am , {noun}, Everyones know that I am the {symbol}, of {sport}. In my mind, I am always the {adjective} Suiiiii!!!!"
+madlibs = f" I am , {noun}, Everyones know that I am the {symbol}, of {sport}. In my mind, I am always the {adjective} Suiiiiiiiiiiiiiiiiii!!!!!!"
 print(madlibs)
