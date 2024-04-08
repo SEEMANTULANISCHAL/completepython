d4c5ec997b935d29036cefd4d4569c4a21d486f2
SEEMANTULANISCHAL <nischal200216@gmail.com>
2024-04-08 23:16:40 +0530

    array

---
+++ Object_Oriented_Programming/array.py
@@ -0,0 +1,15 @@
+# An array is used to store large data of same type in a single file or container
+
+cars = ["Porsche", "BMW", "Audi", "Buggati", "Lambo"]
+
+#Show the car name Buggati
+print(cars[3])
+
+#Print the length of array
+print(len(cars))
+
+#Add a new car "Benz"
+cars.append("Benz")
+
+#Show array
+print(cars)
